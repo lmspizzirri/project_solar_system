69c154812dd9fe9ad8ba3abb1f839364a00cca44
lmspizzirri <lm.spizzirri@gmail.com>
2022-12-05 09:43:40 -0500

    Req 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title'
 
 class App extends React.Component {
   render(){
     return (
       <>
-      <Header />;
-      <SolarSystem />;
+      <Header />
+      <SolarSystem />
+      <Title />
       </>
     )
   }
