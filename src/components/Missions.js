4967141a0d54e804d1fcb93c2eebd5621d264077
lmspizzirri <lm.spizzirri@gmail.com>
2022-12-05 10:15:04 -0500

    Req 8

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component{
     render(){
         return(
+        <>
+        <Title headline="Missões"/>
         <div data-testid="missions"></div>
+        </>
         )
     }
 }
